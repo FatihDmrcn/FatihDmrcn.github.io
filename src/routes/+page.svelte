576e9f676809fdf596e60499d7d6894e1f65cb81
FatihDmrcn <fatihdmrcn@outlook.com>
2024-11-01 20:43:45 +0100

    minor changes

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,7 +1,11 @@
+<script lang="ts">
+    import q from "$lib/q.svg";
+</script>
+
 <svelte:head>
     <title>FatihDmrcn</title> 
 </svelte:head>
 
-<h1>Welcome!</h1>
+<h1>Welcome</h1>
 <dev>Content for this page will be provided soon.</dev>
 
