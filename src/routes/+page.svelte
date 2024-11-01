0ad3d4e83195c7ddd5ace6f2546a6cc4d708e10b
FatihDmrcn <fatihdmrcn@outlook.com>
2024-11-01 16:32:26 +0100

    started small designs

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,2 +1,7 @@
-<h1>Welcome to SvelteKit</h1>
-<p>Visit <a href="https://svelte.dev/docs/kit">svelte.dev/docs/kit</a> to read the documentation</p>
+<svelte:head>
+    <title>FatihDmrcn</title> 
+</svelte:head>
+
+<h1>Welcome!</h1>
+<dev>Content for this page will be provided soon.</dev>
+
