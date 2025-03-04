667eabb1e976135162f24a809f33c783c556a2c8
FatihDmrcn <fatihdmrcn@outlook.com>
2025-03-04 15:05:37 +0100

    added new badges

--- src/lib/components/badge.svelte
+++ src/lib/components/badge.svelte
@@ -1,16 +1,8 @@
 <script lang="ts">
-    let {
-        src,
-        name,
-        inline = false,
-    }: { src: string; name: string; inline?: boolean } = $props();
+    let { src, name }: { src: string; name: string } = $props();
 </script>
 
-<div
-    class="badge badge-neutral {inline
-        ? 'badge-xs sm:badge-md'
-        : 'badge-md sm:badge-lg'} font-bold indent-0"
->
+<div class="badge badge-neutral badge-xs sm:badge-md font-bold indent-0">
     <img {src} class="h-9/10 aspect-auto" alt={name} />
     {name}
 </div>
--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -4,15 +4,24 @@
     import CardContainer from "$lib/components/card_container.svelte";
     import BtnCarousel from "$lib/components/btn_carousel.svelte";
     import Badge from "$lib/components/badge.svelte";
+    import LiAcademia from "$lib/components/li_academia.svelte";
 
     import profile from "$lib/fig/profile.jpg";
 
     import rust from "$lib/logos/rust.svg";
+    import tauri from "$lib/logos/tauri.svg";
     import python from "$lib/logos/python.svg";
     import ts from "$lib/logos/ts.svg";
     import svelte from "$lib/logos/svelte.svg";
+    import tailwindcss from "$lib/logos/tailwindcss.svg";
     import freecad from "$lib/logos/freecad.svg";
-    import LiAcademia from "$lib/components/li_academia.svelte";
+    import siemens from "$lib/logos/siemens.svg";
+    import postgresql from "$lib/logos/postgresql.svg";
+    import docker from "$lib/logos/docker.svg";
+    import numpy from "$lib/logos/numpy.svg";
+    import pandas from "$lib/logos/pandas.svg";
+    import pytorch from "$lib/logos/pytorch.svg";
+    import matplotlib from "$lib/logos/matplotlib.svg";
 
     const len_01 = 2;
     let pos_01 = $state(0);
@@ -29,7 +38,7 @@
             pos_01 = scroll_horizontal(e, len_01);
         }}
     >
-        <CardContainer id={"about_me_0"} title={"About me,.."}>
+        <CardContainer id={"about_me_0"} title={"Who I am"}>
             <div class="avatar float-right">
                 <div class="mask mask-hexagon opacity-80 w-30 sm:w-48">
                     <img class="" src={profile} alt="profile" />
@@ -38,8 +47,8 @@
             <div class="text-justify indent-6 sm:indent-12">
                 I'm a mechanical engineer - who previously chose the path of
                 economics - with honed skills in programming; especially in
-                <Badge src={rust} name={"Rust"} inline /> &
-                <Badge src={python} name={"Python"} inline />. By combining
+                <Badge src={rust} name={"Rust"} /> &
+                <Badge src={python} name={"Python"} />. By combining
                 these skills with my engineering expertise, I can move beyond
                 siloed thinking and deliver high-quality results across both
                 disciplines.
@@ -49,17 +58,47 @@
                 inquiries, feel free to contact me.
             </div>
         </CardContainer>
-        <CardContainer id={"about_me_1"} title={"..my skills,.."}>
-            <div class="w-full flex flex-row flex-wrap gap-2">
-                <Badge src={freecad} name={"FreeCAD"} />
-                <Badge src={rust} name={"Rust"} />
-                <Badge src={python} name={"Python"} />
-                <Badge src={ts} name={"TypeScript"} />
-                <Badge src={svelte} name={"Svelte"} />
-            </div>
+        <CardContainer id={"about_me_1"} title={"What I use"}>
+            <ul class="list">
+                <li class="list-row">
+                    Applications
+                    <div class="list-col-wrap">
+                        <Badge src={siemens} name={"SolidEdge"} />
+                        <Badge src={freecad} name={"FreeCAD"} />
+                        <Badge src={docker} name={"Docker"} />
+                    </div>
+                </li>
+                <li class="list-row">
+                    Languages
+                    <div class="list-col-wrap">
+                        <Badge src={rust} name={"Rust"} />
+                        <Badge src={python} name={"Python"} />
+                        <Badge src={ts} name={"TypeScript"} />
+                    </div>
+                </li>
+
+                <li class="list-row">
+                    Packages / Framworks
+                    <div class="list-col-wrap">
+                        <Badge src={tauri} name={"Tauri"} />
+                        <Badge src={numpy} name={"NumPy"} />
+                        <Badge src={pandas} name={"pandas"} />
+                        <Badge src={pytorch} name={"PyTorch"} />
+                        <Badge src={matplotlib} name={"Matplotlib"} />
+                    </div>
+                </li>
+                <li class="list-row">
+                    Web
+                    <div class="list-col-wrap">
+                        <Badge src={svelte} name={"Svelte"} />
+                        <Badge src={tailwindcss} name={"TailwindCSS"} />
+                        <Badge src={postgresql} name={"PostgreSQL"} />
+                    </div>
+                </li>
+            </ul>
         </CardContainer>
-        <CardContainer id={"about_me_2"} title={"..and my background."}>
-            <ul class="flex-1 list sm:text-lg">
+        <CardContainer id={"about_me_2"} title={"My background"}>
+            <ul class="list sm:text-lg">
                 <LiAcademia
                     degree={"M.Sc. Mechanical Engineering"}
                     university={"Technical University Braunschweig"}
